969332c44acedeb7c1d4b32341264ea5abbd2e97
NOur <werteni11thug@gmail.com>
2022-04-06 11:14:41 +0100

    Commit : TP1 Exercie 2

--- TP_1_java_EE/src/isetj/servlets/CompteurServlet.java
+++ TP_1_java_EE/src/isetj/servlets/CompteurServlet.java
@@ -1,41 +1,32 @@
 package isetj.servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class CompteurServlet
- */
-@WebServlet("/CompteurServlet")
+
+
 public class CompteurServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public CompteurServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+      
+	 // Declaration 
+	 int i = 0;
+	 
+	 public void init() {
+		 
+		 i=1; 
+	 }
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		PrintWriter out = response.getWriter(); 
+		
+		out.println("nombre de visit : "+i); 
+	
+	    i++; 
 	}
 
 }
